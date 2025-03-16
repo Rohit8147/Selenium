4bdcab026fea05ab3e1e9017c110db6ff94d2403
rohit git <1998rohitkuma@gmail.com>
2025-03-16 22:41:22 +0530

    test file

---
+++ firstProject/test.py
@@ -0,0 +1,14 @@
+class car:
+    def __init__(self,brand,colour):
+        self.brand=brand
+        self.colour=colour
+
+    def show_details(self):
+        print("Car Brand is:", self.brand, "& Car colour is:",self.colour)
+
+car1=car("Tata","Red")
+car2=car("Ford","Blue")
+
+car1.show_details()
+car2.show_details()
+
